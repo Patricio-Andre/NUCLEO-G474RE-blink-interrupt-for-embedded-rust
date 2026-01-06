baf6f8a08616af672448e1858bd10b953b3859bd
André Patricio de Sousa <a260363@dac.unicamp.br>
2026-01-06 00:59:36 -0300

    Interrupções e acesso a variáveis através de Mutex funcionando - Específico Cortex-M

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,24 @@
 
 // Deny warnings and unsafe code to simplify teaching and testing.
 // #![deny(warnings)]
-#![deny(unsafe_code)]
+// #![deny(unsafe_code)]
 // `no_main`: use the entry point provided by `cortex-m-rt`.
 #![no_main]
 // `no_std`: embedded environment without the standard library.
 #![no_std]
 
-
 // Import convenience traits for configuring pins and clocks.
 use hal::prelude::*;
 // Access device peripheral structures from the HAL.
 use hal::stm32;
+use stm32g4xx_hal::gpio::ExtiPin;
+use stm32g4xx_hal::gpio::Floating;
+use stm32g4xx_hal::gpio::Input;
+use stm32g4xx_hal::gpio::gpioc;
+
+// Example HAL structure
+
+use stm32g4xx_hal::syscfg::SysCfgExt;
 // Alias the HAL crate for consistent usage in the code.
 use stm32g4xx_hal as hal;
 
@@ -31,6 +38,23 @@ use defmt;
 
 use defmt_rtt as _;
 
+// Configuring interrupts
+
+use core::cell::{Cell, RefCell};
+
+use cortex_m::interrupt::Mutex;
+
+use stm32g4xx_hal::gpio::SignalEdge as SignalEdge;
+
+use stm32g4xx_hal::interrupt;
+
+// Alias for pin button
+type ButtonPin = gpioc::PC13<Input<Floating>>;
+
+// Setting Mutex for interupts
+static G_BUTTON: Mutex<RefCell<Option<ButtonPin>>> = Mutex::new(RefCell::new(None));
+// Setting Mutex for Delay, initial Value 2 seconds
+static G_DELAYMS: Mutex<Cell<u32>> = Mutex::new(Cell::new(2000_u32));
 
 // Minimal panic handler for `no_std` embedded programs.
 #[panic_handler]
@@ -39,61 +63,81 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+
 // Application entry point.
 #[entry]
 fn main() -> ! {
     // Acquire access to microcontroller peripherals.
     // `take()` returns `Some(Peripherals)` only once; it will fail if
     // peripherals have already been taken elsewhere.
-    let dp = stm32::Peripherals::take().expect("cannot take peripherals");
-
+    let mut dp = stm32::Peripherals::take().expect("cannot take peripherals");
+    // taking core peripherals necessary to SYST
+    let cp = stm32::CorePeripherals::take().unwrap();
     // Build the Reset & Clock Control (RCC) configuration.
     let mut rcc = dp.RCC.constrain();
     // Hardware initialization.
-
     // Split GPIOA and GPIOC for pin configuration.
     let gpioa = dp.GPIOA.split(&mut rcc);
     let gpioc = dp.GPIOC.split(&mut rcc);
-
+    // Setting clocks
+    // consumes the configuration!
+    let clocks = rcc.clocks;
+    let mut delay = cp.SYST.delay(&clocks);
+    // Configure Button Pin for Interrupts
+    
     // Configure PA5 as push-pull output — LED pin on Nucleo boards.
     let mut led = gpioa.pa5.into_push_pull_output();
     // Configure PC13 as input. No need to me mutable, we're only reading it.
-    let button = gpioc.pc13.into_floating_input();
+    let mut button = gpioc.pc13.into_floating_input();
     
-    // Delay initial value
-    let mut delay_value = 100_000_u32;
+    
+    // 1) Promote SYSCFG structure to HAL to be able to configure interrupts
+    let mut syscfg = dp.SYSCFG.constrain();
+    // 2) Make button an interrupt source
+    button.make_interrupt_source(&mut syscfg);
+    // 3) Make button an interrupt source
+    button.trigger_on_edge(&mut dp.EXTI, SignalEdge::Rising);
+    // 4) Enable gpio interrupt for button
+    button.enable_interrupt(&mut dp.EXTI);
+    // First unsafe code 🥳
+    // Enable the external interrupt in the NVIC by passing the button interrupt number
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(stm32g4xx_hal::interrupt::EXTI15_10);
+    }
+    // Now that button is configured, move button into global context
+    cortex_m::interrupt::free(|cs| {
+        G_BUTTON.borrow(cs).replace(Some(button));
+    });
 
-    // Initially, led is setted low
-    led.set_low().unwrap();
-    defmt::info!("All Set");
-    // Main loop: toggle the LED with simple busy-wait delays.
+    // Checking the initial value of the Delay using mutex to access a restrict area avoiding concurrency
+    cortex_m::interrupt::free(|cs| {
+        defmt::info!("Delay Initial Value: {}", G_DELAYMS.borrow(cs).get());
+    });
+
     loop {
-        // Call delay funcion and update delay variable once done
-        delay_value = loop_delay(delay_value, &button);
-        
-        // Toggle LED
-        defmt::info!("Toggle! Delay Value: {}", delay_value);
-        led.toggle().unwrap();
+        led.set_high().unwrap();
+        delay.delay_ms(cortex_m::interrupt::free(|cs| G_DELAYMS.borrow(cs).get()));
+
+        led.set_low().unwrap();
+        delay.delay_ms(cortex_m::interrupt::free(|cs| G_DELAYMS.borrow(cs).get()));
     }
 }
 
-// Delay Function
-fn loop_delay<P: InputPin>(mut delay_value: u32, button: &P) -> u32 {
-    let mut change_delay: u8 = 0;
-    // Loop for until value of del
-    for _i in 1..(delay_value) {
-        if button.is_high().unwrap_or(false) {
-            // Para mostrar o efeito do bouncing do botão no terminal
-            defmt::info!("Bounce!");
-            change_delay = 1;
+
+#[interrupt]
+fn EXTI15_10() {
+    // Start a Critical Section
+    cortex_m::interrupt::free(|cs| {
+        // Obtain Access to Delay Global Data and Adjust Delay
+        G_DELAYMS
+            .borrow(cs)
+            .set(G_DELAYMS.borrow(cs).get()/2);
+        if G_DELAYMS.borrow(cs).get() < 125_u32 {
+            G_DELAYMS.borrow(cs).set(2000_u32);
         }
-    }
-    if change_delay == 1 {
-        // If button pressed decrease the delay value
-        delay_value -= 25_000_u32;
-        if delay_value < 25_000_u32 {
-            delay_value = 100_000_u32;
-        }
-    }
-    delay_value
+        defmt::info!("Delay Atual: {} ms", G_DELAYMS.borrow(cs).get());
+        // Obtain access to Global Button Peripheral and Clear Interrupt Pending Flag
+        let mut button = G_BUTTON.borrow(cs).borrow_mut();
+        button.as_mut().unwrap().clear_interrupt_pending_bit();
+    });
 }
